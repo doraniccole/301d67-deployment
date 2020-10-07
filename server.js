f359f6b1cf2ab243dbea1fca7111110ff3c67c7f
DORA BLAKEY DIANA <DORANICCOLE@YAHOO.COM>
2020-10-07 16:30:31 -0600

    managing server js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 'use strict';
-const { response } = require('express');
+//const { response } = require('express');
 const express = require('express');
 //pulls in the express object so we can use it's methods
 const app = express();
@@ -7,17 +7,18 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
 
-app.get('/user.data', (request, response) => {
-    response.send('this worked!');
+//app.get('/user.data', (request, response) => {
+   // response.send('this worked!');
+//});
+
+app.get('/', (request, response) => {
+    response.sendFile('/.public/index.html');
 });
 
-app.get('/', request, response) => {
-    response.sendFile('/.public/index.html');
-
-    app.get('/user-data', (request, response) => {
-        response.json({ username: 'bnates', ps: 12345, location: seattle});
+app.get('/user-data', (request, response) => {
+        response.json({ username: 'doraniccole', pw: '12345', location: 'slc'});
     });
-}
+
 //sets up my server to accept incoming traffic at a specific port
 app.listen (PORT, () => {
     console.log(`listening on ${PORT}`);
